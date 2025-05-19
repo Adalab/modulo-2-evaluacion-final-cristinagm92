70688e867c632db9930de5235121b33fbde9a53b
cris <christty_92@hotmail.com>
2025-05-19 22:55:51 +0200

    jsvariables

---
+++ script.js
@@ -0,0 +1,6 @@
+const searchForm = document.querySelector(".search-box");
+const searchInput = document.querySelector(".search-input");
+const resultsContainer = document.querySelector(".results-grid");
+const favoritesList = document.querySelector(".favorites-list");
+const clearFavsButton = document.querySelector(".clear-favorites-button");
+const resetButton = document.querySelector(".reset-button");
